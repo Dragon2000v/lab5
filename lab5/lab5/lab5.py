c8cb93ead073defc445c7944b5d1ce17e0e624b5
Vitaly <shmatko064@gmail.com>
2021-05-17 12:35:43 +0300

    Код приведён в порядок

--- lab5/lab5/lab5.py
+++ lab5/lab5/lab5.py
@@ -1,12 +1,25 @@
-n = int(input("Введите розмер матрици: "))
-a =[list(map(int, input("Введите строку: ").split())) for i in range(n)]
-for i in range(n):
-    for j in range(n):
-        a[j].append(a[i][j])
-for k in range(n):
-    for i in range(n):
-        a[i].pop(0)
-print("\n")
-print("Т:")
-for i in range(len(a)):
-    print(*a[i])
+class Matrix_input(object):
+    
+
+    def Matrix_len(n,a):
+       for i in range(n):
+           for j in range(n):
+               a[j].append(a[i][j])
+       for k in range(n):
+           for i in range(n):
+               a[i].pop(0)
+       Transposition.Trans(a)
+
+
+class Transposition(object):
+
+    def Trans(a):
+        for i in range(len(a)):
+            print(*a[i])
+
+
+x = int(input("Введите розмер: "))
+y =[list(map(int, input("Введите строку: ").split())) for i in range(x)]
+Matrix_input.Matrix_len(x,y)
+
+
