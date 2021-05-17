458d78b853a98f88f8e427d97f8f17e402c446e9
Vitaly <shmatko064@gmail.com>
2021-05-17 11:25:48 +0300

    Убрали модуль numpy

--- lab5/lab5/lab5.py
+++ lab5/lab5/lab5.py
@@ -1,11 +1,11 @@
-import numpy as np
-
-arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
-print(arr)
-
-print(arr.T)
-
-theArray = np.array([['a','b','c'],['d','e','f'],['g','h','i']])
-print(theArray) 
-
-print(theArray.T)
+n = int(input("Введите розмер матрици"))
+a =[list(map(int, input("Введите строку:").split())) for i in range(n)]
+for i in range(n):
+    for j in range(n):
+        a[j].append(a[i][j])
+for k in range(n):
+    for i in range(n):
+        a[i].pop(0)
+print("\n")
+for i in range(len(a)):
+    print(*a[i])
