2bbb58ebb1802d06094d442329b821b6e036a104
Vitaly <shmatko064@gmail.com>
2021-05-17 11:09:24 +0300

    Добавили модуль numpy

---
+++ lab5/lab5/lab5.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr)
+
+print(arr.T)
+
+theArray = np.array([['a','b','c'],['d','e','f'],['g','h','i']])
+print(theArray) 
+
+print(theArray.T)
